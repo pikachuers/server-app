4de8e13db9ee5ce1796d576395cde535689537b9
willypt <wilwol@windowslive.com>
2014-12-25 23:34:25 +0700

    s

---
+++ app/database/migrations/2014_12_25_152358_create_collections_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollectionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('collections', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('library_id');
+			$table->unsignedInteger('book_id');
+			$table->integer('book_count')->default('0');
+
+			$table->timestamps();
+		});
+		Schema::table('collections', function(Blueprint $table)
+		{
+			$table->foreign('library_id')->references('id')->on('libraries');
+			$table->foreign('book_id')->references('id')->on('books');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('collections');
+		Schema::drop('intertransactions');
+		Schema::drop('libraries');
+		Schema::drop('books');
+		Schema::drop('status');
+	}
+
+}
---
+++ app/database/migrations/2014_12_25_161637_rollback_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RollbackTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('intertransactions');
+		Schema::drop('collections');
+		Schema::drop('libraries');
+		Schema::drop('books');
+		Schema::drop('status');
+	}
+
+}
