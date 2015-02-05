fa80bf91562bd7c19bf1f02e434b26f7260af5b1
willypt <wilwol@windowslive.com>
2015-02-05 20:44:17 +0700

    h

--- app/views/menubar.blade.php
+++ app/views/menubar.blade.php
@@ -5,22 +5,22 @@
 	  		Dashboard<!--/h3-->
 	  	</div>
 	  	<div class="panel-body">
-			<div class="btn-group col-md-3">
+	<!-- 		<div class="btn-group col-md-3">
 			 	<button type="button" class="btn btn-primary col-md-12">
 			    	Buku 
 			  	</button>
-			</div>
+			</div> -->
 			<div class="btn-group col-md-6">
 			 	<button type="button" class="btn btn-primary col-md-12">
 			    	Perpustakaan 
 			  	</button>
 			</div>
-			<div class="btn-group col-md-3">
+<!-- 			<div class="btn-group col-md-3">
 			 	<button type="button" class="btn btn-primary col-md-12">
 			    	Staf
 			  	</button>
 			</div>
-	  	</div>
+ -->	  	</div>
 	</div>
 </center>
 @append
