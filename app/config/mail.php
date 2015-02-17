b2cf3f13d037eb893158e6d165aed90504fb3afd
willypt <wilwol@windowslive.com>
2015-02-17 22:02:52 +0700

    s

--- app/config/mail.php
+++ app/config/mail.php
@@ -3,11 +3,12 @@
 return array(
 	'driver' => 'smtp',
 	'host' => 'smtp.gmail.com',
-	'port' => 465,
+	'port' => 587,
 	'from' => array('address' => 'eliclib.email.notifier@gmail.com', 'name' => "Notifier ". Config::get('serverperpustakaan.namaserver')),
 	'encryption' => 'tls',
 	'username' => 'eliclib.email.notifier@gmail.com',
 	'password' => 'eliclibemail',
 	'sendmail' => '/usr/sbin/sendmail -bs',
 	'pretend' => false,
+	
 );
