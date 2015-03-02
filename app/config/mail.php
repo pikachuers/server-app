daf19a742f6ed329ce3f6593f7cb211012c3e615
willypt <wilwol@windowslive.com>
2015-03-02 14:39:14 +0700

    s

--- app/config/mail.php
+++ app/config/mail.php
@@ -4,11 +4,8 @@ return array(
 	'driver' => 'smtp',
 	'host' => 'smtp.mandrillapp.com',
 	'port' => 587,
-	'from' => array('address' => 'eliclib.email.notifier@gmail.com', 'name' => "Notifier ". Config::get('serverperpustakaan.namaserver')),
+	//'from' => array('address' => 'eliclib.email.notifier@gmail.com', 'name' => "Notifier ". Config::get('serverperpustakaan.namaserver')),
 	'encryption' => 'tls',
 	'username' => 'eliclib.email.notifier@gmail.com',
 	'password' => 'oWnI-LxjlJJp5d1bTl74AQ',
-	'sendmail' => '/usr/sbin/sendmail -bs',
-	'pretend' => false,
-
 );
